0e4b81e639879bad07895a8d19ef181416159f25
Leandro Collares <leco@uvic.ca>
2020-02-09 15:16:38 -0300

    Updates

---
+++ precache-manifest.97475d70cebe6d3463905fe8d52bf38e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca7cc570b1c6655000dcaae5028e3e2c",
+    "url": "/infant-mortality-in-africa/index.html"
+  },
+  {
+    "revision": "e6d2eb499654ba41a68e",
+    "url": "/infant-mortality-in-africa/static/css/main.17be240a.chunk.css"
+  },
+  {
+    "revision": "62d8b1f6f691d91e0a09",
+    "url": "/infant-mortality-in-africa/static/js/2.21d99601.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/infant-mortality-in-africa/static/js/2.21d99601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6d2eb499654ba41a68e",
+    "url": "/infant-mortality-in-africa/static/js/main.50cd5f57.chunk.js"
+  },
+  {
+    "revision": "ec27ba493b8560f64118",
+    "url": "/infant-mortality-in-africa/static/js/runtime-main.31545391.js"
+  },
+  {
+    "revision": "99f069d57d599e4cfe2173bf59152ded",
+    "url": "/infant-mortality-in-africa/static/media/imr.99f069d5.csv"
+  }
+]);
